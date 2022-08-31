147188d6564271cfa75d1fe99d25259eb83dec5f
Lucas Saliés Brum <sistematico@gmail.com>
2022-08-31 06:09:28 -0400

    Update automático

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/' : '/feriado/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? 'public' : 'dist',
   plugins: [vue()]
 })
