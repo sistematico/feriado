ab86a8e657f5899e59079a304d9bc2b427c05122
Lucas Saliés Brum <sistematico@gmail.com>
2022-08-31 05:50:59 -0400

    Update automático

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // base: process.env.BASE_URL,
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.BASE_URL,
+  base: process.env.BASE_URL,
   plugins: [vue()]
 })
