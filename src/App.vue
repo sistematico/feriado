f6050e0e109c100ddd02ccd4facb90dd5a42c871
Lucas Saliés Brum <sistematico@gmail.com>
2022-08-31 08:08:03 -0400

    Update automático

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { afterDates, weekDay } from './logic'
+// import Card from './components/Card'
+import { afterDates, weekDay, diffDays, formatDate } from './logic'
 
 const feriado = ref()
 const data = ref()
 const dia = ref()
 const imagem = ref()
+const diff = ref()
 
 onMounted(() => {
   fetch("feriados.json", {mode: 'cors'})
@@ -14,19 +16,21 @@ onMounted(() => {
     .then((feriados) => {
       const f = afterDates(feriados)
       feriado.value = f.name
-      data.value = f.date
+      data.value = formatDate(f.date)
       dia.value = weekDay(f.date)
+      diff.value = diffDays(f.date)
       imagem.value = f.image
     })
 })
 </script>
 
 <template>
-<a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" :src="imagem" alt="">
-    <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ feriado }}</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ data }} - {{ dia }}</p>
-    </div>
-</a>
+  <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <!-- <img class="object-contain w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" :src="imagem" alt=""> -->
+      <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-96 md:rounded-none md:rounded-l-lg" :src="imagem" :alt="feriado">
+      <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-xl md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ feriado }}</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ data }} - {{ dia }} - faltam {{ diff }} dias</p>
+      </div>
+  </a>
 </template>
--- src/logic/index.js
+++ src/logic/index.js
@@ -1,9 +1,33 @@
+const padTo2Digits = (num) => {
+  return num.toString().padStart(2, '0')
+}
+
+export const formatDate = (d) => {
+  let date = new Date(d)
+
+  return [
+    padTo2Digits(date.getUTCDate()),
+    padTo2Digits(date.getUTCMonth() + 1),
+    date.getFullYear(),
+  ].join('-');
+}
+
 export const afterDates = (dates) => {
   let current = new Date()
-
+  
   return dates.filter((d) => {
     return new Date(d.date) - current >= 0
   })[0]
+}
+
+export const diffDays = (d) => {
+  let now = new Date()
+  let day = new Date(d)
+
+  let diff = day.getTime() - now.getTime()
+  let days = Math.floor(diff / (1000 * 3600 * 24))
+
+  return days > 0 ? days + 1 : days
 }
 
 export const weekDay = (wday) => {
