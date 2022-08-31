0f5517e78c96efd61aed221f8f61933d08ff9522
Lucas Saliés Brum <sistematico@gmail.com>
2022-08-31 05:49:51 -0400

    Update automático

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// base: process.env.MODE == 'staging' ? '/feriado/' : '/', 
+// base: import.meta.env.BASE_URL,
+// base: process.env.BASE_URL,
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.MODE != 'staging' ? '/feriado/' : '/', 
+  base: import.meta.env.BASE_URL,
   plugins: [vue()]
 })
