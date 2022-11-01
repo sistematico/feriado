d0984bd2bda55a4d43551119d4c4c880727785fe
Lucas Saliés Brum <sistematico@gmail.com>
2022-11-01 02:50:15 -0400

    Update automático

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# Vue 3 + Vite
+# 🌎 [Feriado](https://sistematico.github.io/feriado)
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+<p align='center'>
+  <img src='https://github.com/sistematico/lucasbrum.net/raw/main/public/favicon/favicon.svg' alt='Feriado' width='300' />
+</p>
 
-## Recommended IDE Setup
+### 🤖 CI/CD
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+[![CD](https://github.com/sistematico/feriado/actions/workflows/pages.yml/badge.svg)](https://github.com/sistematico/feriado/actions/workflows/pages.yml)
+
+### 📰 Descrição
+
+Um site para mostrar o próximo feriado o <b>[Vite](https://vitejs.dev)</b><sup><em>(com foco na velocidade)</em></sup><br>
+
+### 🎉 Demonstração
+
+- [Github Pages](https://sistematico.github.io/feriado)
+
+### 👏 Ajude
+
+Se o meu trabalho foi útil de qualquer maneira, considere doar qualquer valor através do das seguintes plataformas:
+
+[![LiberaPay](https://img.shields.io/badge/LiberaPay-gray?logo=liberapay&logoColor=white&style=flat-square)](https://liberapay.com/sistematico/donate) [![PagSeguro](https://img.shields.io/badge/PagSeguro-gray?logo=pagseguro&logoColor=white&style=flat-square)](https://pag.ae/bfxkQW) [![ko-fi](https://img.shields.io/badge/ko--fi-gray?logo=ko-fi&logoColor=white&style=flat-square)](https://ko-fi.com/K3K32RES9) [![Buy Me a Coffee](https://img.shields.io/badge/Buy_Me_a_Coffee-gray?logo=buy-me-a-coffee&logoColor=white&style=flat-square)](https://www.buymeacoffee.com/sistematico) [![Open Collective](https://img.shields.io/badge/Open_Collective-gray?logo=opencollective&logoColor=white&style=flat-square)](https://opencollective.com/sistematico) [![Patreon](https://img.shields.io/badge/Patreon-gray?logo=patreon&logoColor=white&style=flat-square)](https://patreon.com/sistematico)
+
+
+[![GitHub Sponsors](https://img.shields.io/github/sponsors/sistematico?label=Github%20Sponsors)](https://github.com/sponsors/sistematico)
--- src/App.vue
+++ src/App.vue
@@ -8,29 +8,33 @@ const data = ref()
 const dia = ref()
 const imagem = ref()
 const diff = ref()
+const wikipedia = ref('https://pt.wikipedia.org/wiki/Portal:Hist%C3%B3ria/Neste_dia')
 
 onMounted(() => {
-  fetch("feriados.json", {mode: 'cors'})
+  fetch('feriados.json', { mode: 'cors' })
     .then((response) => response.json())
     .then((data) => data.feriados)
     .then((feriados) => {
       const f = afterDates(feriados)
+      let icon = f.name != 'Finados' ? '🎉' : '✝️'
       feriado.value = f.name
+      wikipedia.value = `https://pt.wikipedia.org/wiki/${f.name}`
       data.value = formatDate(f.date)
       dia.value = weekDay(f.date)
-      diff.value = diffDays(f.date)
-      imagem.value = f.image
+      diff.value = diffDays(f.date) == 0 ? `${icon} é hoje!` : 'Faltam ' + diffDays(f.date) + ' dias.'
+      imagem.value = f.image ? f.image : 'party.svg'
     })
 })
 </script>
 
 <template>
-  <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <!-- <img class="object-contain w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" :src="imagem" alt=""> -->
-      <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-96 md:rounded-none md:rounded-l-lg" :src="imagem" :alt="feriado">
-      <div class="flex flex-col justify-between p-4 leading-normal">
-          <h5 class="mb-2 text-xl md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ feriado }}</h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ data }} - {{ dia }} - faltam {{ diff }} dias</p>
-      </div>
+  <a :href="wikipedia" target="_blank" class="flex flex-col items-center bg-white rounded-lg border shadow-lg md:flex-row md:w-96 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" :src="imagem" :alt="feriado" />
+    <div class="w-full text-center p-4 leading-normal">
+      <h3 class="mb-2 text-xl md:text-2xl font-bold text-gray-900 dark:text-white">{{ feriado }}</h3>
+      <h6 class="-mt-3 mb-2 text-sm md:text-sm italic tracking-tight text-white-800/25 dark:text-white/25">{{ data }}</h6>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ dia }}</p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ diff }}</p>
+    </div>
   </a>
 </template>
